42dd446d55e4069e9071380c252b8e6d29a42d51
Wsylq <sylqw@atomicmail.io>
2026-05-29 01:32:33 +0530

    bump versionName to 0.4.7 for v0.4.7 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.4.5"
+        versionName = "0.4.7"
 
         testInstrumentationRunner = "com.aeonreader.HiltTestRunner"
     }
