b285d835ddc54a4b2d9bfe4ab5682e8e27a9ef2c
Wsylq <sylqw@atomicmail.io>
2026-05-29 01:21:29 +0530

    sync versionName with release tag (0.4.5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.4.4"
+        versionName = "0.4.5"
 
         testInstrumentationRunner = "com.aeonreader.HiltTestRunner"
     }
