c6354ec968e686918fa7441da090ca048aad29dd
Wsylq <sylqw@atomicmail.io>
2026-06-02 23:11:50 +0530

    bump version to 0.10.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.10.10"
+        versionName = "0.10.11"
 
         testInstrumentationRunner = "com.aeonreader.HiltTestRunner"
     }
